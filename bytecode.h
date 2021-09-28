bad55c448dff98e23cca5e17ba8b9b733b0bc74e
joku432 <poistajalol@hotmail.com>
2021-09-28 17:57:31 +0300

    something.

--- bytecode.h
+++ bytecode.h
@@ -2,6 +2,7 @@
 #define CBASED_BYTECODE_H
 
 #include "visitor.h"
+#include <cstdint>
 
 namespace frontend {
 
@@ -12,7 +13,15 @@ class visitor;
  * Type of a bytecode node.
  */
 enum class bytecode_type {
-
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    ASSIGN,
+    CALL,
+    RET,
+    LOOP,
+    IF
 };
 
 /**
@@ -22,29 +31,79 @@ enum class bytecode_type {
 class bytecode : public visitable {
 public:
     bytecode();
-    virtual void accept(const visitor& v) override {}
+    virtual void accept(const bytecode_visitor& v) override {}
 };
 
 class number_ir : public bytecode {
 public:
-    void accept(const visitor& v) override {
-
-    }
-private:
+    int64_t value;
     size_t size;
-};
-
-/**
- * Call expression
- */
-class call_ir : public bytecode {
-public:
-    void accept(const visitor& v) override {
-
+    void accept(const bytecode_visitor& v) override {
+        v.visit(*this);
     }
-
 };
 
+class add_instr : public bytecode {
+private:
+    std::unique_ptr<number_ir> m_num1;
+    std::unique_ptr<number_ir> m_num2;
+public:
+    add_instr(std::unique_ptr<number_ir> num1,
+              std::unique_ptr<number_ir> num2)
+              : m_num1(std::move(num1)),
+              m_num2(std::move(num2)) {}
+
+    void accept(const bytecode_visitor& v) override {
+        v.visit(*this);
+    }
+};
+
+class sub_instr : public bytecode {
+private:
+    std::unique_ptr<number_ir> m_num1;
+    std::unique_ptr<number_ir> m_num2;
+public:
+    sub_instr(std::unique_ptr<number_ir> num1,
+              std::unique_ptr<number_ir> num2)
+              : m_num1(std::move(num1)),
+              m_num2(std::move(num2)) {}
+
+    void accept(const bytecode_visitor& v) override {
+        v.visit(*this);
+    }
+};
+
+class mul_instr : public bytecode {
+private:
+    std::unique_ptr<number_ir> m_num1;
+    std::unique_ptr<number_ir> m_num2;
+public:
+    mul_instr(std::unique_ptr<number_ir> num1,
+              std::unique_ptr<number_ir> num2)
+              : m_num1(std::move(num1)),
+              m_num2(std::move(num2)) {}
+
+    void accept(const bytecode_visitor& v) override {
+        v.visit(*this);
+    }
+};
+
+class div_instr : public bytecode {
+private:
+    std::unique_ptr<number_ir> m_num1;
+    std::unique_ptr<number_ir> m_num2;
+public:
+    div_instr(std::unique_ptr<number_ir> num1,
+            std::unique_ptr<number_ir> num2)
+            : m_num1(std::move(num1)),
+              m_num2(std::move(num2)) {}
+
+    void accept(const bytecode_visitor& v) override {
+        v.visit(*this);
+    }
+};
+
+class
 
 }
 
